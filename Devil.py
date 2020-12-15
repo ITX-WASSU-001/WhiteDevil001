a3f7f0d8bb8de9fd5917914c1ff0c7669619b8a8
ITX-WASSU-001 <76050140+ITX-WASSU-001@users.noreply.github.com>
2020-12-15 20:41:43 +0500

    Update and rename zeroboy.py to Devil.py

--- Devil.py
+++ Devil.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•ITX - WASSU -OFFICAL------•◈•
+|__\~~) •||•ITX - DEVIL---------------•◈•
+|__(-----\  •◈•------SAM-TECH--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mItx wassu
+\033[1;96mInstagram \033[1;93m: \033[1;M.devil001
+\033[1;96mFacebook  \033[1;93m: \033[1; M.devil001
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/ITX-WASSU-001
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mItx wassu
+\033[1;96mInstagram \033[1;93m: \033[1;92mM.devil001
+\033[1;96mFacebook  \033[1;93m: \033[1;92m M.devil001
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://ITX-WASSU-001
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Wassu"
+CorrectPassword = "devil"
 
 loop = 'true'
 while (loop == 'true'):
